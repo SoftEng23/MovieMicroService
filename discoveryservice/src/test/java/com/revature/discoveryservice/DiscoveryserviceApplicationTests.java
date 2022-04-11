5dd989639fe0a97877029d555e93cabc53ba42d5
Wilson <99214724+SoftEng23@users.noreply.github.com>
2022-04-11 13:09:34 -0700

    Add files via upload
    initial commit

---
+++ discoveryservice/src/test/java/com/revature/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
